fbae5a6b0593c91c4a398c2a1593542a221305df
Jordon Bedwell <jordon@envygeeks.io>
2016-08-11 19:21:50 -0500

    Fix #5233: Increase our ability to detect Windows.
    This increases our ability to detect Windows, and to detect Windows+Bash.  It also adds a message to Windows for users who try to "--watch", also noting to to them to check out the Windows ticket so eventually somebody pings us if this issue is fixed. /cc @TAGraves

--- lib/jekyll/utils/platforms.rb
+++ lib/jekyll/utils/platforms.rb
@@ -13,16 +13,42 @@ module Jekyll
         end
       end
 
+      #
+
+      def windows?
+        !!(RbConfig::CONFIG["host_os"] =~ %r!mswin|mingw|cygwin!i || \
+          proc_version =~ %r!microsoft!i)
+      end
+
+      #
+
+      def linux?
+        RbConfig::CONFIG["host_os"] =~ %r!linux! && \
+          proc_version !~ %r!microsoft!i
+      end
+
       # Provides windows?, linux?, osx?, unix? so that we can detect
       # platforms. This is mostly useful for `jekyll doctor` and for testing
       # where we kick off certain tests based on the platform.
 
-      { :windows? => %r!mswin|mingw|cygwin!, :linux? => %r!linux!, \
-          :osx? => %r!darwin|mac os!, :unix? => %r!solaris|bsd! }.each do |k, v|
+      { :osx? => %r!darwin|mac os!, :unix? => %r!solaris|bsd! }.each do |k, v|
         define_method k do
           !!(
             RbConfig::CONFIG["host_os"] =~ v
           )
+        end
+      end
+
+      #
+
+      private
+      def proc_version
+        @cacahed_proc_version ||= begin
+          Pathutil.new(
+            "/proc/version"
+          ).read
+        rescue
+          nil
         end
       end
     end
